39e431878936a50acfc42e4b61fbb8fcc5d14179
Tom Tobar <codetombomb@gmail.com>
2021-04-20 17:35:08 -0500

    add comments to curly braces. remove solution

--- 1-Update-State-Restaurant-Flyer/src/App.js
+++ 1-Update-State-Restaurant-Flyer/src/App.js
@@ -2,124 +2,73 @@ import './App.css';
 import React, { Component } from 'react';
 import Dots from "./Components/Dots";
 import TemplateLogo from "./Components/TemplatateLogo"
+import DummyData from "./DummyData"
 
 const RestaurantURL = "https://random-data-api.com/api/restaurant/random_restaurant"
 
 class App extends Component {
   constructor() {
     super()
-    this.state = {
-      restaurantId: "",
-      name: "-Restaurant Name-",
-      type: "-Restaurant type-",
-      description: "This is where the restaurant description goes",
-      review: "This is where the restaurant review goes",
-      logo: "https://dummyimage.com/500x500/c4c4c4/000000.png&text=This+is+where+the+logo+goes.",
-      phoneNumber: "(999) 867-5309 x999",
-      address: "1400 Lavaca St 7th Floor, Austin, TX 78701",
-      hours: {
-        monday: {
-          opens_at: "Opens @",
-          closes_at: "Closes @"
-        },
-        tuesday: {
-          opens_at: "Opens @",
-          closes_at: "Closes @"
-        },
-        wednesday: {
-          opens_at: "Opens @",
-          closes_at: "Closes @"
-        },
-        thursday: {
-          opens_at: "Opens @",
-          closes_at: "Closes @"
-        },
-        friday: {
-          opens_at: "Opens @",
-          closes_at: "Closes @"
-        },
-        saturday: {
-          opens_at: "Opens @",
-          closes_at: "Closes @"
-        },
-        sunday: {
-          opens_at: "Opens @",
-          closes_at: "Closes @"
-        },
-      }
-    }
+    this.state = {...DummyData}
   }
-
 
   handleGetRestaurantInfoClick = () => {
-    fetch(RestaurantURL)
-    .then(resp => resp.json())
-    .then(data => this.handleUpdateState(data))
-  }
-
-  handleUpdateState(data){
-    this.setState({
-      restaurantId: data.id,
-      name: data.name,
-      type: data.type,
-      description: data.description,
-      review: data.review,
-      logo: data.logo,
-      phoneNumber: data.phone_number,
-      address: data.address,
-      hours: {...data.hours}
-    })
+    // We will need to do something here to get new restaurant data
   }
 
   render() {
     return (
       <div className="App">
         <Dots />
-        <h1 className="name">{this.state.name}</h1>
+
+        <h1 className="name">{/* The restaurant name goes here */}</h1>
+
         <TemplateLogo />
-        <h2 className="type">The Best {this.state.type}</h2>
+
+        <h2 className="type">The Best {/* The restaurant type goes here */}</h2>
+
         <div className="logo-container">
-          <img className="logo" src={`${this.state.logo}?${this.state.restaurantId}`} alt="Restaurant"></img> {/* By loading the src using a query string, we force a re-render of the image. If not, the image would stay the same due to it being cached. */}
+          <img className="logo" src={`${this.state.logo}?${this.state.id}`} alt="Restaurant"></img> 
           <hr />
           <div className="description">
-            <p>{this.state.description}</p>
+            <p>{/* The restaurant description goes here */}</p>
           </div>
         </div>
         <div className="review-hour-wrapper">
           <div className="info-container">
             <h2>Review</h2>
             <div className="section">
-              <p>"{this.state.review}"</p>
+              <p>"{/* The restaurant review goes here */}"</p>
             </div>
           </div>
           <div className="info-container">
             <h2>Hours</h2>
             <div className="section">
               <p>Monday</p>
-              <p className="open-close">{this.state.hours.monday.opens_at} - {this.state.hours.monday.closes_at}</p>
+              <p className="open-close">{/* Restaurant hours */} - {/* Restaurant hours */}</p>
               <p>Tuesday</p>
-              <p className="open-close">{this.state.hours.monday.opens_at} - {this.state.hours.monday.closes_at}</p>
+              <p className="open-close">{/* Restaurant hours */} - {/* Restaurant hours */}</p>
               <p>Wednesday</p>
-              <p className="open-close">{this.state.hours.monday.opens_at} - {this.state.hours.monday.closes_at}</p>
+              <p className="open-close">{/* Restaurant hours */} - {/* Restaurant hours */}</p>
               <p>Thursday</p>
-              <p className="open-close">{this.state.hours.monday.opens_at} - {this.state.hours.monday.closes_at}</p>
+              <p className="open-close">{/* Restaurant hours */} - {/* Restaurant hours */}</p>
               <p>Friday</p>
-              <p className="open-close">{this.state.hours.monday.opens_at} - {this.state.hours.monday.closes_at}</p>
+              <p className="open-close">{/* Restaurant hours */} - {/* Restaurant hours */}</p>
               <p>Saturday</p>
-              <p className="open-close">{this.state.hours.saturday.opens_at} - {this.state.hours.saturday.closes_at}</p>
+              <p className="open-close">{/* Restaurant hours */} - {/* Restaurant hours */}</p>
               <p>Sunday</p>
-              <p className="open-close">{this.state.hours.sunday.opens_at} - {this.state.hours.sunday.closes_at}</p>
+              <p className="open-close">{/* Restaurant hours */} - {/* Restaurant hours */}</p>
             </div>
           </div>
         </div>
         <div className="contact-info-wrapper">
           <div className="info-wrapper">
             <img src="https://www.iconsdb.com/icons/preview/gray/phone-68-xxl.png" className="icon" alt="Phone Icon"></img>
-            <p>{this.state.phoneNumber}</p>
+            <p>{/* This is where the restaurant phone number goes */}</p>
           </div>
           <div className="info-wrapper">
             <img src="https://www.iconsdb.com/icons/preview/gray/pin-5-xxl.png" className="icon" alt="Pin Icon"></img>
-            <p>{this.state.address}</p>
+            <p>{/* This is where the restaurant address goes */}</p>
           </div>
         </div>
         <Dots />
