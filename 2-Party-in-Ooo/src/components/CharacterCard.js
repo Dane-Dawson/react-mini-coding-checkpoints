eba702a77d5a8ff2316ad11d5732d850dbb4c183
Your Name <you@example.com>
2021-04-20 18:42:24 -0500

    adventure time ready for testing

---
+++ 2-Party-in-Ooo/src/components/CharacterCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function CharacterCard(props) {
+  return (
+    <div>
+      <img
+        onClick={null /* You might want something here */}
+        className="character-card"
+        src={null /* Remove null, then character image goes here */}
+      />
+    </div>
+  );
+}
+
+export default CharacterCard;
